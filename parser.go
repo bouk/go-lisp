a0a10edc019a544e07ee91ffeb2c24f9d793c4bc
Bouke van der Bijl <boukevanderbijl@gmail.com>
2013-03-20 15:41:57 +0100

    Add Parser

---
+++ parser.go
@@ -0,0 +1,45 @@
+package lisp
+
+import (
+	"fmt"
+	"io"
+)
+
+type TreeNode interface {
+	Interpret() (n int, err error)
+}
+
+type ValueNode struct {
+	Value int
+}
+
+func (node *ValueNode) Interpret() (n int, err error) {
+	return node.Value, nil
+}
+
+type FunctionNode struct {
+	Name string
+	Args []*TreeNode
+}
+
+func (node *FunctionNode) Interpret() (n int, err error) {
+	f, exists := registeredFunctions[node.Name]
+	if !exists {
+		return 0, fmt.Errorf("Function %q not found", node.Name)
+	}
+
+	args := make([]int)
+	for index, child := range node.Args {
+		value, err := child.Interpret()
+		if err != nil {
+			return 0, err
+		}
+		args = append(args, value)
+	}
+
+	return f(args)
+}
+
+func Parse(input io.Reader) *TreeNode {
+
+}
