6702387f6c171cfb2934c6be31227dca316bde61
Bouke van der Bijl <boukevanderbijl@gmail.com>
2013-03-20 15:39:49 +0100

    Add function registering

---
+++ functions.go
@@ -0,0 +1,12 @@
+package lisp
+
+type Argument int
+type Function func(args []int) (result int, err error)
+
+var registeredFunctions = make([string]*Function)
+
+func registerFunction(names []string, f *Function) {
+	for _, name := range names {
+		registeredFunctions[name] = f
+	}
+}
