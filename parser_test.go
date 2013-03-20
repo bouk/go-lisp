d6b56558aa3b7a0770265b97db7181c84381dbbd
Bouke van der Bijl <boukevanderbijl@gmail.com>
2013-03-20 15:40:37 +0100

    Add parser test

---
+++ parser_test.go
@@ -0,0 +1,53 @@
+package lisp
+
+import (
+	"strings"
+	"testing"
+)
+
+var parserTestcases = []struct {
+	input  string
+	output TreeNode
+}{
+	{
+		"(+ 1 2)",
+		&FunctionNode{
+			Name: "+",
+			Args: {
+				&ValueNode{1},
+				&ValueNode{2},
+			},
+		},
+	},
+	{
+		"(+ (- 2 2) (* 3 4))",
+		&FunctionNode{
+			Name: "+",
+			Args: {
+				FunctionNode{
+					Name: "-",
+					Args: {
+						&ValueNode{2},
+						&ValueNode{2},
+					},
+				},
+				FunctionNode{
+					Name: "*",
+					Args: {
+						&ValueNode{3},
+						&ValueNode{4},
+					},
+				},
+			},
+		},
+	},
+}
+
+func TestParser(t *testing.T) {
+	for i, testcase := range parserTestcases {
+		output := Parse(strings.NewReader(testcase.input))
+		if output != testcase.output {
+			t.Errorf("%d %q failed", i)
+		}
+	}
+}
